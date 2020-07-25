8786378b819e559f9fb595df035b8967c0d44851
t_kirifu <uniuniworld777@gmail.comu>
2020-07-26 03:03:56 +0900

    no message

---
+++ FakeCallApp/Test/TestVC.swift
@@ -0,0 +1,28 @@
+//
+//  TestVC.swift
+//  FakeCallApp
+//
+//  Created by Takahiro Kirifu on 2020/07/26.
+//  Copyright © 2020 Takahiro Kirifu. All rights reserved.
+//
+
+import UIKit
+import CallKit
+
+class TestVC: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func call(_ sender: Any) {
+        
+        
+        
+    }
+    
+}
